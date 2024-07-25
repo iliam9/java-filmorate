07982aa0756f3daff97a63a1619f5fc7238f9654
Ilya M <ilkafa93@yandex.ru>
2024-07-25 19:33:08 +0300

    11

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -1,30 +1,32 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class ErrorHandler {
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNotFound(final NotFoundException exception) {
-        return Map.of("error", exception.getMessage());
+    public ErrorResponse handleNotFound(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidation(final ValidationException exception) {
-        return Map.of("error", exception.getMessage());
+    public ErrorResponse handleValidation(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleUnknownException(Throwable exception) {
-        return Map.of("error", exception.getMessage());
+    public ErrorResponse handleUnknownException(final Exception e) {
+        log.error(String.format("Исключение %s, текст %s", Exception.class, e.getMessage()));
+        return new ErrorResponse(e.getMessage());
     }
 }
